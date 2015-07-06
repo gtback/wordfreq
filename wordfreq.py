a76f88205ab2c227f882f78dd413510dc0e95806
Greg Back <gback@mitre.org>
2015-07-05 22:49:50 -0500

    Parameterize number of words to output.

--- wordfreq.py
+++ wordfreq.py
@@ -25,14 +25,16 @@ def sort_words(words):
     """Sort the word data from most frequent to least frequent."""
     return sorted(words.items(), key=operator.itemgetter(1), reverse=True)
 
-def output_results(word_freqs, outstream):
+def output_results(word_freqs, outstream, count=10):
     """Output 10 most frequent words."""
-    for x in word_freqs[:10]:
+    for x in word_freqs[:count]:
         outstream.write(str(x) + '\n')
 
 def parse_args():
     """Parse arguments from the command line."""
     parser = argparse.ArgumentParser("Count the frequency of words in text.")
+    parser.add_argument('-c', '--count', type=int, default=10,
+                        help="Number of words to report. Default: 10")
     parser.add_argument('infile', nargs="?", help="Source text. Default: STDIN")
     parser.add_argument('outfile', nargs="?", help="Result destination. Default: STDOUT")
 
@@ -48,10 +50,10 @@ def main():
     words = count_words(data)
     word_freqs = sort_words(words)
     if not args.outfile:
-        output_results(word_freqs, outstream=sys.stdout)
+        output_results(word_freqs, outstream=sys.stdout, count=args.count)
     else:
         with open(args.outfile, 'w') as outfile:
-            output_results(word_freqs, outstream=outfile)
+            output_results(word_freqs, outstream=outfile, count=args.count)
 
 if __name__ == '__main__':
     main()
