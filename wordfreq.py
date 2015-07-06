288a3b8375cc53528124714a908a6fa330447706
Greg Back <gback@mitre.org>
2015-07-05 22:33:52 -0500

    Read from STDIN and Write to STDOUT by default.

--- wordfreq.py
+++ wordfreq.py
@@ -24,19 +24,24 @@ def sort_words(words):
     """Sort the word data from most frequent to least frequent."""
     return sorted(words.items(), key=operator.itemgetter(1), reverse=True)
 
-def output_results(word_freqs, outfile):
+def output_results(word_freqs, outstream):
     """Output 10 most frequent words."""
-    with open(outfile, 'w') as f:
-        for x in word_freqs[:10]:
-            f.write(str(x) + '\n')
-            print(x)
+    for x in word_freqs[:10]:
+        outstream.write(str(x) + '\n')
 
 def main():
     """Main wordfreq function."""
-    data = read_text(infile=sys.argv[1])
+    if len(sys.argv) < 2:
+        data = sys.stdin.read()
+    else:
+        data = read_text(infile=sys.argv[1])
     words = count_words(data)
     word_freqs = sort_words(words)
-    output_results(word_freqs, outfile=sys.argv[2])
+    if len(sys.argv) < 3:
+        output_results(word_freqs, outstream=sys.stdout)
+    else:
+        with open(sys.argv[2], 'w') as outfile:
+            output_results(word_freqs, outstream=outfile)
 
 if __name__ == '__main__':
     main()
