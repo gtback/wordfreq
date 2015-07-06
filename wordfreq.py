2b429080c3bc23fa6178b10c286b506ada9492f3
Greg Back <gback@mitre.org>
2015-07-05 21:20:58 -0500

    Wrap body in a main() function.

--- wordfreq.py
+++ wordfreq.py
@@ -5,21 +5,24 @@ wordfreq.py - Count the frequency of words in text.
 import collections
 import operator
 
-# Read text
-with open('gettysburg.txt') as f:
-    data = f.read()
+def main():
+    # Read text
+    with open('gettysburg.txt') as f:
+        data = f.read()
 
-# Collect occurences of each word
-words = collections.defaultdict(int)
-for word in data.split():
-    word = word.strip('-,.').lower()
-    words[word] += 1
+    # Collect occurences of each word
+    words = collections.defaultdict(int)
+    for word in data.split():
+        word = word.strip('-,.').lower()
+        words[word] += 1
 
-# Sort the word data from most frequent to least frequent.
-word_freqs = sorted(words.items(), key=operator.itemgetter(1), reverse=True)
+    # Sort the word data from most frequent to least frequent.
+    word_freqs = sorted(words.items(), key=operator.itemgetter(1), reverse=True)
 
-# Output 10 most frequent words.
-with open('results.txt', 'w') as f:
-    for x in word_freqs[:10]:
-        f.write(str(x) + '\n')
-        print(x)
+    # Output 10 most frequent words.
+    with open('results.txt', 'w') as f:
+        for x in word_freqs[:10]:
+            f.write(str(x) + '\n')
+            print(x)
+
+main()
