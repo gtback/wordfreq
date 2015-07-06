dafa81e9b41f70851e351b83223ada4b07aed277
Greg Back <gback@mitre.org>
2015-07-05 21:13:59 -0500

    Clean up wordfreq.py
    - Add shebang and make executable.
    - Move imports to the top.
    - Add comments and docstring.
    
    Can now run as:
    
        ./wordfreq.py

--- wordfreq.py
+++ wordfreq.py
@@ -1,12 +1,24 @@
+#!/usr/bin/env python
+"""
+wordfreq.py - Count the frequency of words in text.
+"""
+import collections
+import operator
+
+# Read text
 with open('gettysburg.txt') as f:
     data = f.read()
-import collections
+
+# Collect occurences of each word
 words = collections.defaultdict(int)
 for word in data.split():
     word = word.strip('-,.').lower()
     words[word] += 1
-import operator
+
+# Sort the word data from most frequent to least frequent.
 word_freqs = sorted(words.items(), key=operator.itemgetter(1), reverse=True)
+
+# Output 10 most frequent words.
 with open('results.txt', 'w') as f:
     for x in word_freqs[:10]:
         f.write(str(x) + '\n')
