89b772bb6787d4eb9a44b39a742615a715528ae8
Greg Back <gback@mitre.org>
2015-07-05 21:35:53 -0500

    Parameterize input and output file names with sys.argv
    You now have to run with:
        ./wordfreq.py gettysburg.txt results.txt

--- wordfreq.py
+++ wordfreq.py
@@ -4,10 +4,11 @@ wordfreq.py - Count the frequency of words in text.
 """
 import collections
 import operator
+import sys
 
-def read_text():
+def read_text(infile):
     """Read text"""
-    with open('gettysburg.txt') as f:
+    with open(infile) as f:
         data = f.read()
     return data
 
@@ -23,19 +24,19 @@ def sort_words(words):
     """Sort the word data from most frequent to least frequent."""
     return sorted(words.items(), key=operator.itemgetter(1), reverse=True)
 
-def output_results(word_freqs):
+def output_results(word_freqs, outfile):
     """Output 10 most frequent words."""
-    with open('results.txt', 'w') as f:
+    with open(outfile, 'w') as f:
         for x in word_freqs[:10]:
             f.write(str(x) + '\n')
             print(x)
 
 def main():
     """Main wordfreq function."""
-    data = read_text()
+    data = read_text(infile=sys.argv[1])
     words = count_words(data)
     word_freqs = sort_words(words)
-    output_results(word_freqs)
+    output_results(word_freqs, outfile=sys.argv[2])
 
 if __name__ == '__main__':
     main()
